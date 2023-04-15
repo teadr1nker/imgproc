b6cd37fbfc08a1600f73a541b10375e2f629f4fe
Juhani <arsmir999@gmail.com>
2023-04-15 16:30:24 +0300

    final

--- t7/file1.py
+++ t7/file1.py
@@ -6,11 +6,18 @@ import numpy as np
 image = cv2.imread('images/lines.jpg')
 imageG = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
+# make better
 ret, thresh = cv2.threshold(imageG, 200, 255, cv2.THRESH_BINARY)
+kernel = np.ones((5, 5))
+# thresh = cv2.GaussianBlur(thresh, (3, 3), cv2.BORDER_DEFAULT)
+edges = cv2.dilate(thresh, kernel)
+kernel = np.ones((5, 5))
+edges = cv2.erode(edges, kernel)
+# edges = cv2.Canny(thresh, 100, 200, apertureSize=3)
 
-edges = cv2.Canny(thresh, 100, 200, apertureSize=5)
-
-lines = cv2.HoughLines(edges, 1, np.pi/90, 128)
+# thresh = cv2.GaussianBlur(thresh, (5, 5), cv2.BORDER_DEFAULT)
+# edges = cv2.dilate(edges, kernel)
+lines = cv2.HoughLines(edges, 1, np.pi/360, 143)
 
 if lines is not None:
     for i in range(0, len(lines)):
@@ -22,6 +29,6 @@ if lines is not None:
         y0 = b * rho
         pt1 = (int(x0 + 1000*(-b)), int(y0 + 1000*(a)))
         pt2 = (int(x0 - 1000*(-b)), int(y0 - 1000*(a)))
-        cv2.line(image, pt1, pt2, (0, 255, 0), 2, cv2.LINE_AA)
+        cv2.line(image, pt1, pt2, (0, 255, 0), 1, cv2.LINE_AA)
 
 cv2.imwrite('images/linesHough.jpg', image)
--- t7/file3.py
+++ t7/file3.py
@@ -4,7 +4,7 @@ import numpy as np
 
 image = cv2.imread('images/square_many2.jpg')
 imageG = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-template = image[10:50, 288:328]
+template = image[13:47, 290:325]
 
 w = template.shape[0]
 h = template.shape[1]
@@ -12,12 +12,16 @@ h = template.shape[1]
 squares = cv2.matchTemplate(image,
                             template,
                             cv2.TM_CCOEFF_NORMED)
-threshold = .8
-loc = np.where(squares >= threshold)
+threshold = .93
+loc = np.where(squares > threshold)
+
+squareCount = 0
 
 for pt in zip(*loc[::-1]):
     cv2.rectangle(image, pt,
                   (pt[0]+w, pt[1]+h),
                   (0, 0, 0), 2)
+    squareCount += 1
 
+print(squareCount)
 cv2.imwrite('images/squares.jpg', image)
